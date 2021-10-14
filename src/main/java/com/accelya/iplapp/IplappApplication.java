cc62e7d15260a8834bb342d0afd725421ce8e3ff
xSankeTx <2mhatre3@gmail.com>
2021-10-14 12:07:08 +0530

    Add files via upload

---
+++ src/main/java/com/accelya/iplapp/IplappApplication.java
@@ -0,0 +1,13 @@
+package com.accelya.iplapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accelya/iplapp/IplappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accelya.iplapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
